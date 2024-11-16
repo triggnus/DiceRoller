77f97a2d765cf952110980190195e02d0a90efae
rob <somethingobscure@gmail.com>
2024-11-16 12:22:46 -0600

    Asynchronous V1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::str::FromStr;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::thread::JoinHandle;
 use eframe::egui;
 use egui::Key;
 use eframe::egui::text::LayoutJob;
@@ -59,9 +61,15 @@ fn main() -> eframe::Result
 
 struct DiceApp
 {
-    sides: String,
-    times: String,
+    sides: i32,
+    times: i32,
+    value_a: Arc<Mutex<f64>>,
+    value_b: Arc<Mutex<f64>>,
     results: String,
+    done_a: bool,
+    done_b: bool,
+    handle_a: JoinHandle<()>,
+    handle_b: JoinHandle<()>,
 }
 
 impl Default for DiceApp
@@ -70,10 +78,74 @@ impl Default for DiceApp
     {
         Self
         {
-            sides: "12".to_owned(),
-            times: "1000000".to_owned(),
+            sides: 12,
+            times: 100_000_000,
+            value_a: Arc::new(Mutex::new(0.0)),
+            value_b: Arc::new(Mutex::new(0.0)),
             results: "".to_owned(),
+            done_a: true,
+            done_b: true,
+            handle_a: thread::spawn(move || {}),
+            handle_b: thread::spawn(move || {}),
         }
+    }
+}
+
+impl DiceApp
+{
+
+    fn start_rolling_a(&self) -> JoinHandle<()>
+    {
+        let value = Arc::clone(&self.value_a);
+        let sides = self.sides.clone();
+        let times = self.times.clone();
+
+        let handle = thread::spawn(move || {
+            let mut a = value.lock().unwrap();
+
+            for _ in 0..times
+            {
+                *a += roll(sides) + roll(sides);
+            }
+            println!("Method A - Done.");
+        });
+
+        println!("Method A - Loaded.");
+        handle
+    }
+
+    fn start_rolling_b(&self) -> JoinHandle<()>
+    {
+        let value = Arc::clone(&self.value_b);
+        let sides = self.sides.clone();
+        let times = self.times.clone();
+
+        let handle = thread::spawn(move || {
+            //self.working_b.store(true, std::sync::atomic::Ordering::Relaxed);
+            let mut a = value.lock().unwrap();
+
+            for _ in 0..times
+            {
+                *a += 2.0 * roll(sides);
+            }
+            println!("Method B - Done.");
+            //self.working_b.store(false, std::sync::atomic::Ordering::Relaxed);
+        });
+
+        println!("Method B - Loaded.");
+        handle
+    }
+
+    fn result_a(&self) -> f64
+    {
+        let total = self.value_a.lock().unwrap().clone();
+        return total / self.times as f64;
+    }
+
+    fn result_b(&self) -> f64
+    {
+        let total = self.value_b.lock().unwrap().clone();
+        return total / self.times as f64;
     }
 }
 
@@ -82,57 +154,89 @@ impl eframe::App for DiceApp
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame)
     {
         egui::CentralPanel::default().show(ctx, |ui| {
-            let Self { sides, times, results } = self;
+            let Self { sides, times, .. } = self;
+
+            let sides: i32 = *sides;
+            let times: i32 = *times;
+
+            let sides_str = &mut format!("sides: {}", sides);
+            let times_str = &mut format!("times: {}", times);
 
             ui.heading("Dice Roller");
 
             ui.horizontal(|ui| {
                 let sides_label = ui.label("Sides on Dice: ");
-                ui.text_edit_singleline(sides)
+                ui.text_edit_singleline(sides_str)
                   .labelled_by(sides_label.id);
             });
 
             ui.horizontal(|ui| {
                 let times_label = ui.label("Times to Roll: ");
-                ui.text_edit_singleline(times)
+                ui.text_edit_singleline(times_str)
                   .labelled_by(times_label.id);
             });
 
             //ui.label(format!("Hello '{}', age {}", self.name, self.age));
 
 
-            let s = i32::from_str(sides).unwrap_or(0);
-            let t = i32::from_str(times).unwrap_or(0);
-
             let btn = egui::Button::new("Roll <Enter>").min_size(Vec2::new(80.0, 30.0));
             let r = ui.add(btn);
 
-            if s == 0 || t == 0
+            if sides == 0 || times == 0
             {
                 return;
             }
 
             if r.clicked() || ctx.input(|i| i.key_pressed(Key::Enter))
             {
-                *results = format!("Rolling (2d{}) {} times...\n", sides, times.separate_with_commas());
+                self.done_a = false;
+                self.done_b = false;
+                self.results = "".to_owned();
+               /* let handle_a = self.start_rolling_a();
+                let handle_b = self.start_rolling_b();*/
+
+                self.handle_a = self.start_rolling_a();
+                self.handle_b = self.start_rolling_b();
+
+                /* *results = format!("Rolling (2d{}) {} times...\n", sides, times.separate_with_commas());
 
                 let mut a = 0.0;
-                for _ in 0..t
+                for _ in 0..times
                 {
-                    a = a + roll(s) + roll(s);
+                    a = a + roll(sides) + roll(sides);
                 }
 
-                results.push_str(format!("Average roll: {:.5}\n", a / (t as f64)).as_str());
+                results.push_str(format!("Average roll: {:.5}\n", a / (times as f64)).as_str());
                 results.push_str(format!("Rolling (1d{} x 2) {} times...\n", sides, times.separate_with_commas()).as_str());
 
                 let mut b:f64 = 0.0;
-                for _ in 0..t
+                for _ in 0..times
                 {
-                    b = b + (2.0 * roll(s));
+                    b = b + (2.0 * roll(sides));
                 }
 
-                results.push_str(format!("Average roll: {:.5}\n", b / (t as f64)).as_str());
-                results.push_str(format!("Percent Difference: {:.5}%", (f64::max(a, b) - f64::min(a, b)) / f64::max(a, b) * 100f64).as_str());
+                results.push_str(format!("Average roll: {:.5}\n", b / (times as f64)).as_str());
+                results.push_str(format!("Percent Difference: {:.5}%", (f64::max(a, b) - f64::min(a, b)) / f64::max(a, b) * 100f64).as_str());*/
+            }
+
+            if self.handle_a.is_finished() && !self.done_a
+            {
+                self.done_a = true;
+
+                self.results.push_str(format!("Rolling (2d{}) {} times...\n", sides, times.separate_with_commas()).as_str());
+                self.results.push_str(format!("Average roll: {:.5}\n", self.value_a.lock().unwrap().clone() / (times as f64)).as_str());
+
+                println!("Method A - {}", self.result_a());
+            }
+
+            if self.handle_b.is_finished() && !self.done_b
+            {
+                self.done_b = true;
+
+                self.results.push_str(format!("Rolling (1d{} x 2) {} times...\n", sides, times.separate_with_commas()).as_str());
+                self.results.push_str(format!("Average roll: {:.5}\n", self.value_b.lock().unwrap().clone() / (times as f64)).as_str());
+
+                println!("Method B - {}", self.result_b());
             }
 
            /* ui.centered_and_justified(move |ui| {
@@ -141,7 +245,7 @@ impl eframe::App for DiceApp
             });*/
             egui::ScrollArea::vertical().show(ui, |ui| {
                 let mut job = LayoutJob::single_section(
-                    self.results.to_owned(),
+                    self.results.clone(),
                     egui::TextFormat::default(),
                 );
 
@@ -150,6 +254,7 @@ impl eframe::App for DiceApp
                 // NOTE: `Label` overrides some of the wrapping settings, e.g. wrap width
                 ui.label(job);
             });
+            ctx.request_repaint();
         });
     }
 }
